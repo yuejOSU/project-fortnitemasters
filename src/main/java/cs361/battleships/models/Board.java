2e8ec9d782b7918a7848b17878ebf95c04a453d8
Garibaed <garibaed@oregonstate.edu>
2018-10-11 12:59:18 -0700

    initialized game board

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -4,12 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
+	 //Private variables
+	private char[][] board;
+
+	 //public variables
+	public static int NUM_ROWS = 10;
+	public static int NUM_COLS = 10;
+
 	public Board() {
 		// TODO Implement
+		this.board = new char[NUM_ROWS][NUM_COLS];
+		this.initializeBoard();
+	}
+
+	public void initializeBoard(){
+			for(int row = 0; row < NUM_ROWS; row++){
+				for(int col = 0; col < NUM_COLS; col++){
+					this.board[row][col] = "";
+				}
+			}
+		}
+
 	}
 
 	/*
