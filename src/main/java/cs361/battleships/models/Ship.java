8bc1c987047082a495d7e2dfc2649a19fd1c5351
Andy Trinh <trinha@os1.engr.oregonstate.edu>
2018-10-11 20:44:24 -0700

    fixed syntax errors and added getters

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ship {
-	private int size:
+	private int size;
 	private String name;
 	@JsonProperty private List<Square> occupiedSquares;
 
