ea4173d1d9a16ab4e2250cdca3ec47290201ac60
garibaed <garibaed@oregonstate.edu>
2018-10-11 22:19:52 -0700

    worked on getters/setters

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -8,17 +8,19 @@ public class Board {
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	 //Private variables
-	private Board[][] board;
+	private Square[][] board;
+	private List<Ship> ships;
 
 	 //public variables
 	public static int NUM_ROWS = 10;
 	public static int NUM_COLS = 10;
 
 	public Board() {
-		// TODO Implement
 
-		this.board = new Board[NUM_ROWS][NUM_COLS];
+		this.board = new Square[NUM_ROWS][NUM_COLS];
 		this.initializeBoard();	//calls a method to initialize the game Board
+		this.createShips();
+		this.recordAttacks();
 	}
 
 	//This method will initialize a 2-Dimensional array of Board Type
@@ -28,9 +30,14 @@ public class Board {
 					this.board[row][col] = "";	//initializes every row and col to blank spaces
 				}
 			}
-		}
-
 	}
+	public void createShips(){
+		ships = new ArrayList<Ship>();
+	}
+	public void recordAttacks(){
+		attacks = new ArrayList<Result>();
+	}
+}
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
@@ -44,25 +51,29 @@ public class Board {
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Result attack(int x, char y) {
-		//TODO Implement
-		return null;
+			Result results = new Result();
+
+			Square square = new Square(x, y);
+
+			results.setLocation(square);
+
+		return results;
 	}
 
 	public List<Ship> getShips() {
-		//TODO implement
-		return null;
+
+		return ships;
 	}
 
 	public void setShips(List<Ship> ships) {
-		//TODO implement
+		this.ships = ships;
 	}
 
 	public List<Result> getAttacks() {
-		//TODO implement
-		return null;
+		return attacks;
 	}
 
 	public void setAttacks(List<Result> attacks) {
-		//TODO implement
+		this.attacks = attacks;
 	}
 }
