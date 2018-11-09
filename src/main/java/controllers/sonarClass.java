88fc220b741ff349bdc8c2a3ffcb22fb97ccd60d
Jason Yue <yuej@oregonstate.edu>
2018-11-09 15:46:13 -0800

    Deleted duplicates in sonarClass.java, edit unit test

--- src/main/java/controllers/sonarClass.java
+++ src/main/java/controllers/sonarClass.java
@@ -16,19 +16,6 @@ public class sonarClass {
     @JsonProperty
     private int sonarCount;
 
-=======
-import com.fasterxml.jackson.annotation.JsonProperty;
-import cs361.battleships.models.Game;
-public class sonarClass {
-    @JsonProperty
-    private Game game;
-    @JsonProperty
-    private int x;
-    @JsonProperty
-    private char y;
-    @JsonProperty
-    private int sonarCount;
-
     public Game getGame()
     {
         return game;
