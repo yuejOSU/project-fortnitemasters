7d6a7f5e293123fb8bca825388951314970edb27
Andy Trinh <trinha@os1.engr.oregonstate.edu>
2018-11-08 17:12:54 -0800

    further impl of CQ and CQ UI and added test for hitting CQ

--- src/test/java/cs361/battleships/models/ShipTest.java
+++ src/test/java/cs361/battleships/models/ShipTest.java
@@ -164,15 +164,15 @@ public class ShipTest {
         assertTrue(minesweeper1.equals(minesweeper2));
         assertEquals(minesweeper1.hashCode(), minesweeper2.hashCode());
     }
-    
+
     @Test
-    public void testHitCaptainsQuarters() {
+    public void testHitCQ() {
         Ship minesweeper = new Ship("MINESWEEPER");
         minesweeper.place('A', 1, true);
         var result = minesweeper.attack(1, 'A');
         assertEquals(AtackStatus.SUNK, result.getResult());
     }
-}
+
 
 
 }
