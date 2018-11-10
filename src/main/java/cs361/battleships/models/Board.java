6831fc1310e4c5e892d9609a6e33899b11787282
Jason Yue <yuej@oregonstate.edu>
2018-11-09 16:06:04 -0800

    Debugged Board.java

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -61,67 +61,6 @@ public class Board {
 		return attackResult;
 	}
 
-	public boolean sonarResult(int x, char y, int numSonars) {
-		// conditional to return false if all sonars are used or no ships have been sunk
-		if(!ifSonar() || numSonars == 0) {
-			return false;
-		}
-		int r = 2;
-		int i;
-		List<Square> squares = new ArrayList<>();
-		// loop to create the radius for the sonar just like in 'game.js'
-		for(i = 0; i < (r + 1); i++) {
-			squares.add(new Square((x - i), y));
-			squares.add(new Square((x + i), y));
-			// typecasting is need for the rest of these because we cannot pass in 'int' for function
-			squares.add(new Square(x, (char)((int)y - i)));
-			squares.add(new Square(x, (char)((int)y + i)));
-			squares.add(new Square((x - 1), (char)((int) y - 1)));
-			squares.add(new Square((x - 1), (char)((int) y + 1)));
-			squares.add(new Square((x + 1), (char)((int) y - 1)));
-			squares.add(new Square((x + 1), (char)((int) y + 1)));
-		}
-		for (Square square: squares) {
-			// this adds the squares that the sonar is used on; if there are square that aren't on the board, then
-			// we just ignore it, but if it is in it, then we add it to sonar list
-			if(square.getRow() <= this.rows && square.getRow() > 0 && ((int)square.getColumn() - 65) < this.cols && ((int)square.getColumn() - 65) >= 0) {
-				this.sonars.add(square);
-			}
-		}
-		return true;
-	}
-
-	private boolean ifSonar() {
-		// checks to see if any ships have been sunk in order to use the sonar weapon
-		for (Result r: this.attacks) {
-			if(r.getResult() == AtackStatus.SUNK) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private Result attack(Square s) {
-		if (attacks.stream().anyMatch(r -> r.getLocation().equals(s))) {
-			var attackResult = new Result(s);
-			attackResult.setResult(AtackStatus.INVALID);
-			return attackResult;
-		}
-		var shipsAtLocation = ships.stream().filter(ship -> ship.isAtLocation(s)).collect(Collectors.toList());
-		if (shipsAtLocation.size() == 0) {
-			var attackResult = new Result(s);
-			return attackResult;
-		}
-		var hitShip = shipsAtLocation.get(0);
-		var attackResult = hitShip.attack(s.getRow(), s.getColumn());
-		if (attackResult.getResult() == AtackStatus.SUNK) {
-			if (ships.stream().allMatch(ship -> ship.isSunk())) {
-				attackResult.setResult(AtackStatus.SURRENDER);
-			}
-		}
-		return attackResult;
-	}
-
 	public boolean sonar(int x, char y, int numSonars) {
 
 		// conditional to return false if all sonars are used or no ships have been sunk
@@ -185,5 +124,5 @@ public class Board {
 	}
 
 	public int getCols() {
-		return this.rows;
+		return this.cols;
 	}
