e7e4714e6515ab0363d22822f13bb363e31d0b8d
Andy Trinh <trinha@os1.engr.oregonstate.edu>
2018-12-01 16:11:43 -0800

    updated tests

--- src/main/java/cs361/battleships/models/submarine.java
+++ src/main/java/cs361/battleships/models/submarine.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class submarine extends Ship{
     public submarine() {
+        this.size = 5;
         this.occupiedSquares = new ArrayList<Square>(); // constructs list
         this.kind = "SUBMARINE"; // sets kind equal to what was passed in
         this.underwater = false;
